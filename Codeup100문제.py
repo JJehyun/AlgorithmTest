eafe4b273d10615eed5a8dca0821e5035633fae9
JJehyun <povt99@gmail.com>
2022-10-26 23:31:20 +0900

    first commit

---
+++ Codeup100문제.py
@@ -0,0 +1,63 @@
+# print("6018번")
+# a,b = input().split(":")
+# print(a,b,sep=":")
+
+
+# print("6019번")
+# a,b,c = input().split(".")
+# print(c,b,a,sep="-")
+
+
+#print("6020번")
+# a,b = input().split("-")
+# print(a,b,sep="")
+
+#print("6021번")
+# a = input()
+# num = 0
+# while num < len(a):
+#     print(a[num])
+#     num = num + 1
+
+#print("6022번")
+# a = input()
+# print(a[0:2],a[2:4],a[4:6])
+
+#print("6023번")
+# a,b,c = input().split(":")
+# print(b)
+
+#print("6024번")
+# a,b = input().split()
+# print(a,b,sep="")
+
+#print("6025번")
+# a,b = input().split()
+# c = int(b) + int(a)
+# print(c)
+
+
+#print(6026반)
+# a = float(input())
+# b = float(input())
+# print(a+b)
+
+#print(6027번)
+# a = int(input())
+# print("%x"%a)
+
+#print(6028번)
+# a = int(input())
+# print("%X"%a)
+
+#print(6029번) - 진수
+# a = int(input(), 16)
+# print("%o"%a)
+
+# print("6030번") - 유니코드 변환
+# a = ord(input())
+# print(a)
+
+#print("6031번")
+# a = int(input())
+# print(chr(a))
