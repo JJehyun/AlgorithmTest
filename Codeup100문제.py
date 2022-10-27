a887091dbd23079652e3a9e0bbcf112b769b7f83
JehyunNoteBook <povt99@gmail.com>
2022-10-27 14:28:35 +0900

    2022/10/27 codeup 6047번 까지

--- Codeup100문제.py
+++ Codeup100문제.py
@@ -61,3 +61,86 @@
 #print("6031번")
 # a = int(input())
 # print(chr(a))
+
+# print("6031번")
+# a = int(input())
+# print(chr(a))
+
+# print("6032번")
+# a = int(input())
+# print(-a)
+
+# print("6033번")  -- ord : 문자를 유니코드로 chr : 유니코드를 문자로
+# a = ord(input())
+# print(chr(a+1))
+
+# print("6034번") 
+# a,b = input().split()
+# c = int(a)
+# d = int(b)
+# print(c-d)
+
+# print("6035번")
+# a,b = input().split()
+# c = float(a)
+# d = float(b)
+# print(c*d)
+
+# print("6036번")
+# a,b = input().split()
+# print(a*int(b))
+
+# print("6037번")
+# a = int(input())
+# b = input()
+# print(a*b)
+
+# print("6038번")
+# a,b = input().split()
+# print(int(a)**int(b))
+
+# print("6039번")
+# a,b = input().split()
+# print(float(a)**float(b))
+
+# print("6040번")
+# a,b = input().split()
+# print(int(a)//int(b))
+
+# print("6041번")
+# a,b = input().split()
+# print(int(a)%int(b))
+
+
+# print("6042번")
+# a = float(input())
+# print(round(a,2))
+# print(format(a,".2f"))
+
+# print("6043번")
+# a,b = input().split()
+# c=float(a)/float(b)
+# print("%.3f"%c)
+
+# print("6044번")
+# a,b = input().split()
+# c = float(a)/float(b)
+# print(int(a)+int(b))
+# print(int(a)-int(b))
+# print(int(a)*int(b))
+# print(int(a)//int(b))
+# print(int(a)%int(b))
+# print("%.2f" %c)
+
+# print("6045번")
+# a,b,c = input().split()
+# d = int(a)+int(b)+int(c)
+# print(d,format(d/3,".2f"))
+
+# print("6046번")
+# a = int(input())
+# print(a*2)
+
+print("6047번")
+a,b = input().split()
+print(int(a)<<int(b))
