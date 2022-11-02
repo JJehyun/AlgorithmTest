35f7538426c1a649e88e2719ec8ae7537fca5da6
JehyunNoteBook <povt99@gmail.com>
2022-11-02 17:28:08 +0900

    2022/11/02 programmers 0단계 풀이

---
+++ programmers 문제 풀이.py
@@ -0,0 +1,43 @@
+# print("몫 구하기 문제")
+# def solution(num1, num2):
+#     answer = num1 // num2 
+#     return answer
+
+# print("나머지 구하기 문제")
+# def solution(num1, num2):
+#     answer = num1 % num2
+#     return answer
+
+# print("두 수의 차")
+# def solution(num1, num2):
+#     answer = num1 - num2
+#     return answer
+
+# print("숫자 비교하기 문제")
+# def solution(num1, num2):
+#     if num1 == num2:
+#         answer = 1
+#     else:
+#         answer = -1
+#     return answer
+
+# print("각도기 문제")
+# def solution(angle):
+#     if angle < 90 : 
+#         answer = 1
+#     elif angle == 90 : 
+#         answer = 2
+#     elif angle < 180 : 
+#         answer = 3
+#     else : 
+#         answer = 4
+#     return answer
+
+# print("짝수의 합 문제")
+# a = int(input())
+# answer = 0
+# for i in range(a+1):
+#     print(i)
+#     if i % 2 == 0:
+#         answer += i
+# print(answer)
