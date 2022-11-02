d9a5758664c62630d60fa2c62fdf72148ac800ad
JJehyun <povt99@gmail.com>
2022-11-02 23:40:15 +0900

    2022/11/02 programmers 문제 풀이

--- programmers 문제 풀이.py
+++ programmers 문제 풀이.py
@@ -41,3 +41,72 @@
 #     if i % 2 == 0:
 #         answer += i
 # print(answer)
+
+# print("배열의 평균값 문제")
+# def solution(numbers):
+#     result = 0
+#     for i in numbers :
+#         result += i
+#     answer = result / len(numbers)
+#     return answer
+
+# print("중복된 숫자 개수")
+# def solution(array, n):
+#     answer = 0
+#     for i in array:
+#         if i == n :
+#             answer += 1 
+#     return answer
+
+# print("중복된 숫자 개수") - 남의 풀이
+# def solution(array, n):
+#     return array.count(n)
+
+# print("양꼬치 문제")
+# def solution(n, k):
+#     answer = (n * 12000) + (k * 2000) - (n//10 * 2000)
+#     return answer
+
+# print("두 수의 나눗셈")
+# def solution(num1, num2):
+#     answer = int(num1 / num2 * 1000)
+#     return answer
+
+# print("머쓱이보다 키 큰사람")
+# def solution(array, height):
+#     answer = 0
+#     for i in array: 
+#         if i > height:
+#             answer += 1
+#     return answer
+
+# print("피자 나눠 먹기")
+# def solution(n):
+#     if (n/7).is_integer():
+#         answer = n // 7
+#     else :
+#         answer = (n // 7) + 1
+#     return answer
+
+# print("편지 문제")
+# def solution(message):
+#     answer = len(message) * 2
+#     return answer
+
+# print("짝수 홀수 갯수 문제")
+# def solution(num_list):
+#     num1 = 0
+#     num2 = 0
+#     for i in num_list:
+#         if i % 2 == 0 :
+#             num1 += 1
+#         else : 
+#             num2 += 1
+#     answer = [num1, num2]
+#     return answer
+
+# print("배열 뒤집기")
+# def solution(num_list):
+#     answer = num_list
+#     answer.reverse()
+#     return answer
