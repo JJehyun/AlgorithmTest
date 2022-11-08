bf5121eefbc69eb2d6064c2214bfe6ec2b2b13b6
JJehyun <povt99@gmail.com>
2022-11-08 22:24:47 +0900

    2022/11/08

--- programmers 문제 풀이.py
+++ programmers 문제 풀이.py
@@ -1,3 +1,255 @@
+# print("6057번")
+# a,b = input().split()
+# if bool(int(a)) == bool(int(b)):
+#     print(True)
+# else:
+#     print(False)
+
+# print("6058번")
+# a, b = input().split()
+# c = bool(int(a))
+# d = bool(int(b))
+# print(not(c or d))
+
+# print("6059번")
+# a=int(input())
+# print(~a)
+
+# print("6060번")      - 비트 단위 연산 and = &
+# a,b = input().split()
+# print(int(a) & int(b))
+
+# print("6061번")
+# a,b = input().split()
+# print(int(a) | int(b))
+
+# print("6062번")
+# a,b = input().split()
+# print(int(a) ^ int(b))
+
+# print("6063번")        -  파이썬 삼항 연산자
+# a,b = input().split()
+# c = int(a) if int(a)>int(b) else int(b)
+# print(c)
+
+# print("6064번")
+# a,b,c = map(int,input().split())
+# print((a if (a<b) else b)if ((a if (a<b) else b)<c) else c)
+
+# print("6065번")
+# a,b,c = map(int,input().split())
+# if a%2==0:
+#     print(a)
+# if b%2==0:
+#     print(b)
+# if c%2==0:
+#     print(c)
+
+# print("6066번")
+# a,b,c = map(int,input().split())
+# if a%2==0:
+#     print("even")
+# else:
+#     print("odd")
+# if b%2==0:
+#     print("even")
+# else:
+#     print("odd")
+# if c%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+# print("6067번")
+# a = int(input())
+# if a<0:
+#     if a%2==0:
+#         print('A')
+#     else : print('B')
+# else:
+#     if a%2==0:
+#         print('C')
+#     else : print('D')
+
+# print("6068번")
+# a = int(input())
+# if a>=90:
+#     print("A")
+# elif a>=70:
+#     print("B")
+# elif a>=40:
+#     print("C")
+# else:
+#     print("D")
+
+# print("6069번")
+# a = input()
+# if a=="A":
+#     print("best!!!")
+# elif a=="B":
+#     print("good!!")
+# elif a=="C":
+#     print("run!")
+# elif a=="D":
+#     print("slowly~")
+# else:
+#     print("what?")
+
+# print("6070번")
+# month = int(input())
+# if month//3==1:
+#     print('spring')
+# elif (month-3)//3==1:
+#     print('summer')
+# elif (month-6)//3==1:
+#     print('fall')
+# else : print('winter')
+
+# print("6071번")
+# a = 1
+# while a !=0:
+#     b = int(input())
+#     if b==0:
+#         a = 0 
+#         break
+#     print(b)
+
+# print("6073번")
+# a = int(input())
+# while a != 0:
+#     print(a)
+#     a = a - 1
+
+# print("6074번")
+# a = int(input())
+# while a != 0:
+#     a = a - 1
+#     print(a)
+
+# print("6075번")
+# a = ord(input())
+# t = ord('a')
+# while t <= a:
+#     print(chr(t),end=" ")
+#     t = t + 1
+
+# print("6075번")
+# a = int(input())
+# num = 0
+# while num <= a:
+#     print(num)
+#     num = num + 1
+
+# print("6076번")
+# a = int(input())
+# for i in range(a+1):
+#     print(i)
+
+
+# print("6077번")
+# a = int(input())
+# s = 0
+# for i in range(1,a+1):
+#     if i%2 == 0:
+#         s += i
+
+# print(s)
+
+# print("6078번")
+# a = 1
+# while a == 1:
+#     b = input()
+#     print(b)
+#     if b == "q":
+#         a = 2
+#         break
+
+# print("6079번")
+# x = int(input())
+# result = 0
+
+# for i in range(1, x+1):
+#   result += i
+#   if result >= x :
+#     print(i)
+#     break
+
+# print("6080번")
+# a,b = input().split()
+# for i in range(1,int(a)+1):
+#     for j in range(1,int(b)+1):
+#         print(i,j)
+
+# print("6081번")
+# a = int(input(),16)
+# for i in range(1,16):
+#     print('%X*%X=%X' %(a,i,a*i))
+
+# print("6083번")
+# count = 0
+# a,b,c = map(int,input().split())
+# for i in range(0,a):
+#     for j in range(0,b):
+#         for k in range(0,c):
+#             print(i , j , k)
+#             count +=1
+
+# print(count)
+
+# print("6084번")
+# h,b,c,s = map(int,input().split())
+# m = (h* b * c * s)/8/1024/1024
+# print('%.1f MB' %m)
+
+# print("6085번")
+# w, h, b = map(int,input().split())
+# print("%.2f MB" %(round(w*h*b/8/1024/1024,2)))
+
+# print("6086번")
+# a = int(input())
+# count = 0
+# while count < a :
+#     count +=1
+#     count += count
+# print(count)
+
+
+
+# print("-----------------------------------------------------------------------------------")
+# print("1402문제")    --순서대로 들어오는 배열 반대로 출력하기- 스택문제!!
+# a = int(input())
+# b = list(map(int,input().split()))
+# b.reverse()
+# for i in b:
+#     print(i , end=" ")
+
+# print("1714문제")    --입력된 숫자열 반대로 출력하기 -- 스택문제!!
+# a = list(map(int,input()))
+# a.reverse()
+# for i in a:
+#     print(i,end="")
+
+# print("2001번")       --그리디 문제!!
+# pasta=[]
+# for i in range(3):
+#     pasta.append(float(input()))
+
+# juice=[]
+# for i in range(2):
+#     juice.append(float(input()))
+
+# result = (min(pasta)+min(juice))*1.1
+# print(format(result,".1f"))
+
+# print("3301번")         -- / 나누기 , % 나머지 , // 몫 그리디 문제
+# a = int(input())
+# num = 0
+# array = [50000,10000,5000,1000,500,100,50,10]
+# for i in array:
+#     num += a//i
+#     a = a % i
+# print(num)
+
 # print("몫 구하기 문제")
 # def solution(num1, num2):
 #     answer = num1 // num2 
@@ -204,4 +456,3 @@
 #     else:
 #         return 2
 
-#ㅁㄴㅇㅁㄴㅇ
