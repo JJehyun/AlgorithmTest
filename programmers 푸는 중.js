280dd5a7da438c7b92902ed0e04418a7a06207fc
JehyunNoteBook <povt99@gmail.com>
2022-11-23 17:52:43 +0900

    2022/11/23 programmers 문제 풀이

--- programmers 푸는 중.js
+++ programmers 푸는 중.js
@@ -143,3 +143,88 @@
 //     return answer;
 // }
 
+//console.log("Lv1 제일 작은 수 제거하기")
+// function solution(arr) {
+//     const index = arr.indexOf(Math.min(...arr));
+//     arr.splice(index, 1);
+//     return arr.length === 0 ? [-1] : arr;
+//   }
+
+// console.log("Lv1 음양 더하기")
+// function solution(absolutes, signs) {
+//     let arr2 =[];
+//     signs.forEach((element,index,array)=>{
+//         element === true ? arr2.push(absolutes[index]) : arr2.push(absolutes[index] * -1)
+//     })
+//     var answer = arr2.reduce((acc,cur)=>acc+cur,0)
+//     return answer;
+// }
+
+// console.log("Lv1 없는 숫자 더하기")
+// function solution(numbers) {
+//     let answer = 0;
+//     for(let i=1; i<10 ;i++){
+//         if(!numbers.includes(i))
+//             answer += i
+//     }
+//     return answer;
+// }
+// console.log("Lv1 배열 더하기")
+// function solution(s) {
+//     return s.length % 2 !== 0
+//       ? s[Math.floor(s.length / 2)]
+//       : s.slice(s.length / 2 - 1, s.length / 2 + 1);
+//   }
+
+// console.log("Lv1 수박수박수박수박수박수?")
+// function solution(n) {
+//     let arr = []
+//     for(i=0; i<n; i++){
+//         if(i%2 == 0){
+//             arr.push("수")
+//         }else{
+//             arr.push("박")
+//         }
+//     }
+//     return arr.join("");
+// }
+
+// console.log("Lv1 내적")
+// function solution(a, b) {
+//     let answer = 0;
+//     a.forEach((element,index,arr)=>{
+//         num = a[index]*b[index]
+//         answer +=num;
+//         num=0
+//     })
+//     return answer;
+// }
+
+// console.log("Lv1 문자열 내림차순으로 배치하기")
+// function solution(s) {
+//     return s
+//       .split("")
+//       .sort()
+//       .reverse()
+//       .join("");
+//   }
+
+// console.log("Lv1 문자열 다루기 기본")
+// function alpha_string46(s){
+//     var regex = /^\d{5}$|^\d{4}$/;
+//     console.log(regex.test(s));
+//   }
+
+// console.log("약수의 개수와 덧셈")
+// function solution(left, right) {
+//     var answer = 0;
+
+//     for (let i = left; i <= right; i++){
+//         if(Number.isInteger(Math.sqrt(i)) === true){
+//             answer -= i
+//         }else{
+//             answer += i
+//         }
+//     }
+//     return answer;
+// }
