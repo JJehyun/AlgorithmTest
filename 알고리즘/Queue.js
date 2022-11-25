48890e18a48c0ddc72416afab3ce0990f0857291
JJehyun <povt99@gmail.com>
2022-11-25 23:56:10 +0900

    javascript 알고리즘 구현

--- 알고리즘/Queue.js
+++ 알고리즘/Queue.js
@@ -67,3 +67,64 @@ class Queue2{
         return this.head.value
     }
 }
+
+//문제
+class Node {
+    constructor(value){
+        this.value;
+        this.next = null;
+    }
+}
+
+class Queue{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+    }
+    //값 추가를 위한 함수
+    enqueue(newValue){
+        const newNode = new Node(newValue);
+        if(this.head === null){
+            this.head = this.tail =newNode;
+            console.log(this.head)
+        }else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
+
+    //값을 제거하는 함수s
+    dequeue(){
+        const value = this.head.value
+        this.head = this.head.next
+        return value;
+    }
+
+    //현재 head의 값을 가져오는 함수
+    peek(){
+        return this.head.value
+    }
+}
+
+
+function solution(priorities,location){
+    const queue = new Queue();
+    for(let i = 0; i < priorities.length;i+=1){
+        queue.enqueue([priorities[i],i]);
+    }
+    priorities.sort((a,b)=> b-a);
+
+    let count = 0;
+    while(true){
+        const currentValue = queue.peek();
+        if(currentValue[0] < priorities[count]){
+            queue.enqueue(queue.dequeue());
+        }else{
+            const value = queue.dequeue();
+            count +=1;
+            if(location === value[1]){
+                return count 
+            }
+        }
+    }
+}
