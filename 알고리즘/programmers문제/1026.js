d1c8dd44e1aabae81c5d6aa3809293425aa9e926
JJehyun <povt99@gmail.com>
2022-11-26 16:37:42 +0900

    11/26

--- 알고리즘/programmers문제/1026.js
+++ 알고리즘/programmers문제/1026.js
@@ -1,16 +1,13 @@
-//그리디
-// const fs = require('fs');
-// let input = fs.readFileSync('dev/stdin').toString().trim().split('\n');
-// input.shift();
-// input = input.map((v) => v.split(' ').map(Number));
+const fs = require('fs');
+let input = fs.readFileSync('dev/stdin').toString().trim().split('\n');
+input.shift();
+input = input.map((v) => v.split(' ').map(Number));
+input[0].sort((a, b) => a - b);
+input[1].sort((a, b) => b - a);
+let answer = 0;
 
-// input[0].sort((a, b) => a - b);
-// input[1].sort((a, b) => b - a);
+input[0].filter((v, idx) => {
+  answer += v * input[1][idx];
+});
 
-// let answer = 0;
-
-// input[0].filter((v, idx) => {
-//   answer += v * input[1][idx];
-// });
-
-// console.log(answer);
+console.log(answer);
